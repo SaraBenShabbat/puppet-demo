48d5d031221b9c80c1a1bf16d88f7676a4b04885
Sara Ben Shabbat <49598750+SaraBenShabbat@users.noreply.github.com>
2020-03-01 21:26:30 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,15 @@
+node default {
+}
+
 node 'puppet-master' {
   include role::master_server
 }
+
+node 'puppet-agent' {
+  include role::app_server
+}
+
+node 'puppet-centos' {
+  include role::db_server
+}
+  

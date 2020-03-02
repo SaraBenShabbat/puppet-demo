bbd2d1937f3f9f2edd804fe3aa45f2752755a1af
Sara Ben Shabbat <49598750+SaraBenShabbat@users.noreply.github.com>
2020-03-02 11:11:51 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,19 +3,23 @@ node default {
 
 node 'puppet-master' {
   include role::master_server
+  file { '/root/README':
+    ensure => file,
+    content => $fqdn,
+   }
 }
 
 node 'puppet-agent' {
   file { '/root/README':
     ensure => file,
-    content => fqdn,
+    content => $fqdn,
    }
 }
 
 node 'puppet-centos' {
   file { '/root/README':
     ensure => file,
-    content => fqdn,
+    content => $fqdn,
    }
 }
   

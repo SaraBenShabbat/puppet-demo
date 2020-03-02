690426ec29178db72229bb32c7ef513ff90ae6c3
Sara Ben Shabbat <49598750+SaraBenShabbat@users.noreply.github.com>
2020-03-02 12:32:26 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,28 +1,48 @@
-node default {
+node default{
 }
 
 node 'puppet-master' {
   include role::master_server
-  file { '/root/SERVER_NAME':
-    ensure => file,
-    content => "Welcome to ${fqdn}!\n",
-    owner => 'root',
-   }
 }
 
 node 'puppet-agent' {
-  include role::master_server
-  file { '/root/SERVER_NAME':
-    ensure => file,
-    content => "Welcome to ${fqdn}!\n",
-   }
 }
 
 node 'puppet-centos' {
-  include role::master_server
-  file { '/root/INFO_TIMEZONE':
-    ensure => file,
-    content => "Now ${timezone}!\n",
-   }
 }
+
+
+
+
+
+
+
+
+#node default {
+#}
+
+#node 'puppet-master' {
+#  include role::master_server
+#  file { '/root/SERVER_NAME':
+#    ensure => file,
+#    content => "Welcome to ${fqdn}!\n",
+#    owner => 'root',
+#   }
+#}
+
+#node 'puppet-agent' {
+#include role::master_server
+#  file { '/root/SERVER_NAME':
+#    ensure => file,
+#    content => "Welcome to ${fqdn}!\n",
+#   }
+#}
+
+#node 'puppet-centos' {
+#  include role::master_server
+#  file { '/root/INFO_TIMEZONE':
+#    ensure => file,
+#    content => "Now ${timezone}!\n",
+#   }
+#}
   

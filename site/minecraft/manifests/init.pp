f599441ebfeea3c3dfc346e0ed039bc6fcb6f107
Sara Ben Shabbat <49598750+SaraBenShabbat@users.noreply.github.com>
2020-03-03 02:47:45 +0200

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,20 @@
+class minecraft {
+
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+    
+  package {'wget':
+    ensure => present,
+  }
+  
+  exec {'wget http://s3.amazonaws.com/Minecraft.Download/versions/1.12.2/minecraft_server.1.12.2.jar -o /opt/minecraft/minecraft_server.jar
+    path => '/usr/bin:/usr/sbin:/bin',
+    provider => shell,
+  }
+  
+  package {'java-1.8.0-openjdk':
+    ensure => present,
+  }
+  
+}  

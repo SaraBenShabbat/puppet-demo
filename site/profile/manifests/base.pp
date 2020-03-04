63127f9d94620315cc8f9f8295fea1bb7bdb94a7
Sara Ben Shabbat <49598750+SaraBenShabbat@users.noreply.github.com>
2020-03-04 12:13:25 +0200

    Update base.pp

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,11 @@ class profile::base {
   user {'admin':
     ensure => present,
   }
+  user { "sarabenshabbat":
+    ensure     => present,
+    uid        => '507',
+    gid        => 'admin',
+    home       => '/home/sarabenshabbat',
+    managehome => true,
+  }
 }
